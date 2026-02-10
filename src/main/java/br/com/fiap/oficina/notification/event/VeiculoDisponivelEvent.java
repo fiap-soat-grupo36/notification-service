ea947a24e7a4b6cec2e662a82104be5f75e5fb74
GrecoMilani <greco_lima@hotmail.com>
2026-02-10 20:28:29 -0300

    Add initial configuration

---
+++ src/main/java/br/com/fiap/oficina/notification/event/VeiculoDisponivelEvent.java
@@ -0,0 +1,7 @@
+package br.com.fiap.oficina.notification.event;
+
+import br.com.fiap.oficina.notification.model.Cliente;
+import br.com.fiap.oficina.notification.model.Veiculo;
+
+public record VeiculoDisponivelEvent(Cliente clienteResumo, Veiculo veiculoResumo, String token) {
+}
