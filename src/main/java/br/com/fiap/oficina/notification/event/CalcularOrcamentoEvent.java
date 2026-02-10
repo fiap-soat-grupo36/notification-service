41ef6ce23291ad896dbbc37a0fabc2c1053e992a
GrecoMilani <greco_lima@hotmail.com>
2026-02-10 20:28:29 -0300

    Add initial configuration

---
+++ src/main/java/br/com/fiap/oficina/notification/event/CalcularOrcamentoEvent.java
@@ -0,0 +1,6 @@
+package br.com.fiap.oficina.notification.event;
+
+import br.com.fiap.oficina.notification.model.OrdemServico;
+
+public record CalcularOrcamentoEvent(OrdemServico ordemServico) {
+}
